904b11e9b5448e02343d0bbdb7cc3363e24fd7ec
Molly B <103689113+mbruns13@users.noreply.github.com>
2022-07-23 17:20:26 -0500

    cleaning up jupyter file and scrape function code

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -3,6 +3,7 @@ from splinter import Browser
 from bs4 import BeautifulSoup
 import requests
 import os
+import time
 from webdriver_manager.chrome import ChromeDriverManager
 
 def scrape():
@@ -14,6 +15,8 @@ def scrape():
     # URL to scrape
     url_news = 'https://redplanetscience.com/'
     browser.visit(url_news)
+    # Let it sleep for 1 second
+    time.sleep(1)
     # html object
     html_news = browser.html
     # Create BeautifulSoup object; parse with 'html.parser'
@@ -28,17 +31,18 @@ def scrape():
     news_p = results_news_p.text
     
     # quit browser scraping for Mars News Site
-    browser.quit()
+    # browser.quit()
     
     # JPL Mars Space Images—Featured Image
     
-    executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
+    # executable_path = {'executable_path': ChromeDriverManager().install()}
+    # browser = Browser('chrome', **executable_path, headless=False)
 
     # URL to scrape
     url_image = 'https://spaceimages-mars.com/'
     browser.visit(url_image)
-
+    # Let it sleep for 1 second
+    time.sleep(1)
     # html object
     html_image = browser.html
     # Create BeautifulSoup object; parse with 'html.parser'
@@ -49,18 +53,12 @@ def scrape():
     featured_image_url = f"{url_image}{results_img['src']}"
     
     # quit browser scraping for JPL Mars Space Images
-    browser.quit()
+    # browser.quit()
     
     # Mars Facts
     
     # URL to scrape
     url_facts = 'https://galaxyfacts-mars.com/'
-    
-    # Retrieve page with the requests module
-    # html_facts = requests.get(url_facts)
-    # Create BeautifulSoup object; parse with 'html.parser'
-    # soup_facts = BeautifulSoup(html_facts.text, 'html.parser')
-    #results_facts = soup_facts.find('table', class_="table-striped")
     
     # Mars facts table
     tables = pd.read_html(url_facts)
@@ -71,15 +69,48 @@ def scrape():
     
     # Mars Hemispheres
     
-    # https://marshemispheres.com/
-    hemisphere_image_urls = [
-        {"title": "Valles Marineris Hemisphere", "img_url": "https://marshemispheres.com/images/b3c7c6c9138f57b4756be9b9c43e3a48_valles_marineris_enhanced.tif_full.jpg"},
-        {"title": "Cerberus Hemisphere", "img_url": "https://marshemispheres.com/images/f5e372a36edfa389625da6d0cc25d905_cerberus_enhanced.tif_full.jpg"},
-        {"title": "Schiaparelli Hemisphere", "img_url": "https://marshemispheres.com/images/3778f7b43bbbc89d6e3cfabb3613ba93_schiaparelli_enhanced.tif_full.jpg"},
-        {"title": "Syrtis Major Hemisphere", "img_url": "https://marshemispheres.com/images/555e6403a6ddd7ba16ddb0e471cadcf7_syrtis_major_enhanced.tif_full.jpg"},
-    ]
+    # executable_path = {'executable_path': ChromeDriverManager().install()}
+    # browser = Browser('chrome', **executable_path, headless=False)
+
+    url_hemispheres = 'https://marshemispheres.com/'
+    browser.visit(url_hemispheres)
+    # Let it sleep for 1 second
+    time.sleep(1)
+    html_hemispheres = browser.html
+    soup_hemispheres = BeautifulSoup(html_hemispheres, 'html.parser')
+
+    headers=[]
+    titles = soup_hemispheres.find_all('h3')
+    #removing the last header 'Back' that we don't need
+    titles = titles[:-1]
+
+    for title in titles:
+        headers.append(title.text)
+
+    #empty list to store image src urls in
+    images=[]
+    count=0
+    for h in headers:
+        #using splinter to find thumbnail images, click through to each link,
+        #then find the src link for each image with class "wide-image"
+        browser.find_by_css('img.thumb')[count].click()
+        images.append(browser.find_by_css('img.wide-image')['src'])
+        browser.back()
+        #counter used to loop through each thumbnail image
+        count=count+1
+
+    #list to store dictionaries in
+    hemisphere_image_urls = []
+    counter = 0
+    for i in images:
+        #appending each title and url to the list of dictionaries
+        hemisphere_image_urls.append({"title":headers[counter],"img_url":images[counter]})
+        counter = counter+1
+
+    browser.quit()
 
     data = {"newsTitle":news_title, "newsParagraph":news_p, "image_url":featured_image_url, "marsTable":mars_facts_table, "hemisphereImages":hemisphere_image_urls}
 
-    return data
+    quit
 
+    return data
